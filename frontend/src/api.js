56c25126dc704e86c3a0c438fbae2c0bee1d0120
HELLOBIBEK-OG <Pritiranjans99@gmail.com>
2026-02-03 10:19:52 +0530

    adding backend api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export const uploadCSV = (file) => {
   const formData = new FormData();
   formData.append("file", file);
-
-  return axios.post("http://localhost:5000/api/upload", formData, {
+//http://localhost:5000/api/upload
+  return axios.post("https://ready-ai-pritiranjan-sahoo.vercel.app/api/upload", formData, {
     headers: { "Content-Type": "multipart/form-data" }
   });
 };
